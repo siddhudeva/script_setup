dbff4da4acda4e6d91bcc69b866f1fe0f0da4ea8
SiddhuDeva <siddhudangeti@gmail.com>
2021-12-12 11:58:28 +0530

    Mongodb added

--- components/Frontend.sh
+++ components/Frontend.sh
@@ -3,29 +3,29 @@
 
 source components/common.sh
 
-yum install nginx -y &>>Log_file
+yum install nginx -y &>>${Log_file}
 Status $? "Nginx installation"
 
-systemctl enable nginx && systemctl start nginx &>>Log_file
+systemctl enable nginx && systemctl start nginx &>>${Log_file}
 Status $? "Nginx services"
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>Log_file
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${Log_file}
 Status $? "Download of project"
 
-rm -rf /usr/share/nginx/html/*  &>>Log_file
+rm -rf /usr/share/nginx/html/*  &>>${Log_file}
 Status  $? "Removed Nginx Files is"
 
-cd /usr/share/nginx/html/ && unzip /tmp/frontend.zip &>>Log_file
+cd /usr/share/nginx/html/ && unzip /tmp/frontend.zip &>>${Log_file}
 Status $? "Unzipping"
 
-mv ./frontend-main/* . && mv ./static/* . &>>Log_file
+mv ./frontend-main/* . && mv ./static/* . &>>${Log_file}
 Status $? "all content moved to Frontend File"
 
-rm -rf frontend-master static README.md &>>Log_file
+rm -rf frontend-master static README.md &>>${Log_file}
 Status $? "Removed Unwanted files is"
 
-mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>Log_file
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>${Log_file}
 Status $? "Moving the conf file to proper Location"
 
-systemctl restart nginx &>>/tmp/Logs &>>Log_file
+systemctl restart nginx &>>/tmp/Logs &>>${Log_file}
 Status $? "Restarting of Nginx is"
--- components/Mongodb.sh
+++ components/Mongodb.sh
@@ -4,3 +4,26 @@
 
 source components/common.sh
 
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+
+yum install -y mongodb-org
+Status $? "MongoDb installation"
+
+systemctl enable mongod && systemctl start mongod
+Status $? "Mongodb enabling and starting"
+
+sed -i "s/127.0.0.0/0.0.0.0/g" /etc/mongod.conf
+Status $? "configaration update"
+
+systemctl restart mongod
+Status $? "restarting of MongoDb"
+
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+Status $? "Schema downloading"
+
+cd /tmp/ && unzip mongodb.zip
+Status $? "Schema downloading"
+cd mongodb-main
+
+mongo < catalogue.js && mongo < users.js
+Status $? "Schema downloading"
