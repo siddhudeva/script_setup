a7db06f23e226936c58fb1014752a3fee9c961f4
SiddhuDeva <siddhudangeti@gmail.com>
2021-12-12 10:34:51 +0530

    common script edited v1.1 log_file added

--- components/Frontend.sh
+++ components/Frontend.sh
@@ -3,29 +3,29 @@
 
 source components/common.sh
 
-yum install nginx -y &>>/tmp/Logs
+yum install nginx -y &>>Log_file
 Status $? "Nginx installation"
 
-systemctl enable nginx && systemctl start nginx &>>/tmp/Logs
+systemctl enable nginx && systemctl start nginx &>>Log_file
 Status $? "Nginx services"
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/Logs
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>Log_file
 Status $? "Download of project"
 
-rm -rf /usr/share/nginx/html/* &>>/tmp/Logs
+rm -rf /usr/share/nginx/html/* &>>/tmp/Logs &>>Log_file
 Status  $? "Removed Nginx Files is"
 
-cd /usr/share/nginx/html/ && unzip /tmp/frontend.zip &>>/tmp/Logs
+cd /usr/share/nginx/html/ && unzip /tmp/frontend.zip &>>Log_file
 Status $? "Unzipping"
 
-mv frontend-main/* . && mv static/* . &>>/tmp/Logs
+mv frontend-main/* . && mv static/* . &>>Log_file
 Status $? "all content moved to Frontend File"
 
-rm -rf frontend-master static README.md &>>/tmp/Logs
+rm -rf frontend-master static README.md &>>Log_file
 Status $? "Removed Unwanted files is"
 
-mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/Logs
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>Log_file
 Status $? "Moving the conf file to proper Location"
 
-systemctl restart nginx &>>/tmp/Logs
+systemctl restart nginx &>>/tmp/Logs &>>Log_file
 Status $? "Restarting of Nginx is"
